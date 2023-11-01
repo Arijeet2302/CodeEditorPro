76b0c3fa044141608e705102f20e5ad4741b60a6
arijeet <arijeetsinha2@gmail.com>
2023-11-01 23:39:39 +0530

    added cors

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const http = require('http');
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(cors("*"));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URL, {
